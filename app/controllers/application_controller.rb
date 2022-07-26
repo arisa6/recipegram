67f177b684fb5d543a0a5cc5b577be72162f66d4
EC2 Default User <ec2-user@ip-172-31-46-86.ap-northeast-1.compute.internal>
2022-07-26 17:20:59 +0000

    ログイン機能の実装とレイアウト調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller? #ユーザーのページにアクセスする前にprivateを実行する
+  
+  
+  private#このcontroller内でしか呼び出されないメソッド
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])   #sign_upの際にusernameを許可するメソッド
+  end
 end
