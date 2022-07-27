07b9ca3618df39669f802b21eba0906ee96f4d65
EC2 Default User <ec2-user@ip-172-31-46-86.ap-northeast-1.compute.internal>
2022-07-27 18:21:33 +0000

    recipe/creat,show実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_one_attached :profile_image  #idは書かない
+  has_many :recipes, dependent: :destroy #userが削除されたら関連づいているレシピも削除される
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   resources :users #6つのroutesが作成される
+  resources :recipes #6つのroutesが作成される
+  
 end
